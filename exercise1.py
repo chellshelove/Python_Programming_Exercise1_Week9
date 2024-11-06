e6e8d21a97c6af931100ad0605f441732a4730b8
chellshelove <chellshelove733@gmail.com>
2024-11-06 10:32:01 +0900

    done

---
+++ exercise1.py
@@ -0,0 +1,11 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"Point({self.x}, {self.y})"
+
+# Example usage
+p1 = Point(3, 4)
+print(p1)
